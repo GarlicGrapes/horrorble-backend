a20dafbd2c1bc8ea8ac502c2bc1c24d3f255ae01
Austin Longo <austinlongocontracting@gmail.com>
2022-11-23 00:34:19 -0500

    updated poster_urls in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,10 +16,10 @@ puts "🎥 Creating Movies..."
 get_out = Movie.create(title: "Get Out", release_year: 2017, director: peele, watched: true, poster_url: "https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg")
 us = Movie.create(title: "Us", release_year: 2019, director: peele, watched: true, poster_url: "https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg")
 hungry_clown_movie = Movie.create(title: "It", release_year: 1986, director: king, watched: false, poster_url: "https://movieposters2.com/images/704489-b.jpg")
-shutin_movie = Movie.create(title: "The Shining", release_year: 1980, director: kubrick, watched: true, poster_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fm.imdb.com%2Ftitle%2Ftt0081505%2Fmediaindex&psig=AOvVaw29qpziahjoSmSgXZohUFiI&ust=1669267238321000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOD1mpfHw_sCFQAAAAAdAAAAABAE")
+shutin_movie = Movie.create(title: "The Shining", release_year: 1980, director: kubrick, watched: true, poster_url: "https://en.wikipedia.org/wiki/File:The_Shining_(1980)_U.K._release_poster_-_The_tide_of_terror_that_swept_America_IS_HERE.jpg")
 not_your_graveyard_movie = Movie.create(title: "Pet Sematary", release_year: 2019, director: king, watched: false, poster_url: "https://m.media-amazon.com/images/M/MV5BZTgxMGQwM2UtZTAxNi00M2JhLTk4ZTgtZmFlODI0NmI3ZDViXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg")
-it_takes_a = Movie.create(title: "The Village", release_year: 2004, director: twisty, watched: true, poster_url: "https://static.wikia.nocookie.net/unbreakablemovie/images/6/64/51XBP8AYNRL.jpg/revision/latest?cb=20170831183256")
-sad_plants = Movie.create(title: "The Happening", release_year: 2008, director: twisty, watched: true, poster_url: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt0949731%2Fmediaindex&psig=AOvVaw03fkVLNsPZ-DziKrozABnM&ust=1669267350466000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCNCN1MzHw_sCFQAAAAAdAAAAABAO")
+it_takes_a = Movie.create(title: "The Village", release_year: 2004, director: twisty, watched: true, poster_url: "https://en.wikipedia.org/wiki/File:The_Village_movie.jpg")
+sad_plants = Movie.create(title: "The Happening", release_year: 2008, director: twisty, watched: true, poster_url: "https://en.wikipedia.org/wiki/File:Happening_poster.jpg")
 
 puts "👹 Creating Monsters..."
 
